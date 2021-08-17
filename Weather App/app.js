ea2377edba9146b80f5351d9b17fa5115630f3ba
Oheneba Dade <65047259+Oheneba-Dade@users.noreply.github.com>
2021-08-17 10:32:06 +0000

    add error handling

--- Weather App/app.js
+++ Weather App/app.js
@@ -3,16 +3,40 @@ const btn = document.querySelector('button');
 const main = document.querySelector('main');
 const APIKey = 'b42cdbcf7b6d473d94c85730211308';
 const body = document.querySelector('body');
-
+const loading = document.getElementById('loading');
+const error = document.getElementById('error');
 
 btn.addEventListener('click', async () => {
-    //remove background image
-    body.classList.remove('imageToggle');
+    //display loading message
+    loading.style.display = 'block'
+
     //reset the page after each new entry
-    main.innerHTML = '';
-    const location = input.value;
-    const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${location}`);
-    displayInfo(res.data);
+    main.innerHTML = ''
+
+    
+    try {
+        if(input.value) {
+            const location = input.value;
+            //remove background image
+            body.classList.remove('imageToggle');
+            const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${location}`);
+            displayInfo(res.data);
+        }
+        else{
+            alert('Please Enter A City!')
+
+        }
+        
+    }
+     catch (error) {
+        loading.style.display = 'none'
+        main.innerHTML = `<span id="error">Sorry, Could Not Find Weather Information For This City</span>`
+    }
+    
+
+    //remove loading message
+    loading.style.display = 'none'
+
     //reset the input box
     input.value = '';
 })
@@ -36,6 +60,7 @@ function displayInfo(place) {
     temperature.textContent = `Temperature (Degrees Celsius) - ${place.current.temp_c}`;
     condition.textContent = `Condition - ${place.current.condition.text}`;
     windSpeed.textContent = `Wind Speed (Kilometres Per Hour) - ${place.current.wind_kph}`;
+    
     
     if (place.current.is_day === 0) {
         document.body.style.backgroundColor = 'rgb(50, 71, 105)';
